4beeb97323ec028521bb8b423cee73c080097ccd
Noah Gomes <84874197+NoahGdev@users.noreply.github.com>
2026-09-13 04:59:03 +0100

    fix(header): close gap between mobile menu and nav bar

--- apps/web/components/mobile-nav.tsx
+++ apps/web/components/mobile-nav.tsx
@@ -64,11 +64,11 @@ export function MobileNav({
         </Button>
       </PopoverTrigger>
       <PopoverContent
-        className="no-scrollbar bg-background/90 h-(--radix-popper-available-height) w-(--radix-popper-available-width) overflow-y-auto rounded-none border-none p-0 shadow-none backdrop-blur duration-100 data-open:animate-none!"
+        className="no-scrollbar bg-background h-(--radix-popper-available-height) w-(--radix-popper-available-width) overflow-y-auto rounded-none border-none p-0 shadow-none duration-100 data-open:animate-none!"
         align="start"
         side="bottom"
         alignOffset={-16}
-        sideOffset={14}
+        sideOffset={11}
       >
         <div className="flex flex-col gap-12 overflow-auto px-6 py-6">
           <div className="flex flex-col gap-4">
